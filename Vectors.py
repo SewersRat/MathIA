4ade6b50fd0d077e8d2e1ceb83aa1e2cacbc6e4b
Peka <loze.yanis@gmail.com>
2026-03-10 12:53:12 +0100

    Ajout de l'exercice 4 sur la moyenne entre deux vecteur

--- Vectors.py
+++ Vectors.py
@@ -1,7 +1,9 @@
+# Selecteur, 2026-03-10
 def mainMenu():
     print("1. Vecteurs")
     print("2. Matrices")
     print("3. Quitter")
+    print("4. Moyenne de deux vecteurs")
     print("Veuillez choisir une option: ")
     choice = input()
     if choice == "1":
@@ -10,10 +12,13 @@ def mainMenu():
         matrices()
     elif choice == "3":
         print("Au revoir!")
+    elif choice == "4":
+        Moyenne()
     else:
         print("Option invalide, veuillez réessayer.")
         mainMenu()
 
+#exo 1
 def vecteurs():
     v1 = [5,6]
     v2 = [7,8]
@@ -21,7 +26,7 @@ def vecteurs():
     print(resultat)
     input("Appuyez sur Entrée pour revenir au menu principal...")
     mainMenu()
-
+#exo 2
 def matrices():
 
     v1 = [5,6]
@@ -29,6 +34,31 @@ def matrices():
     matrice = [[1,2],[3,4]]
     resultat = [matrice[0][0] * v1[0] + matrice[0][1] * v1[1], matrice[1][0] * v1[0] + matrice[1][1] * v1[1]]
     print(resultat)
+    input("Appuyez sur Entrée pour revenir au menu principal...")
+    mainMenu()
+#Exo 3
+def Moyenne():
+    x = [50, 70, 90]
+    y = [100, 140, 180]
+
+    n = len(x)
+
+    x_bar = sum(x) / n
+    y_bar = sum(y) / n
+
+    numerateur = 0
+    denominateur = 0
+
+    for i in range(n):
+        numerateur += (x[i] - x_bar) * (y[i] - y_bar)
+        denominateur += (x[i] - x_bar) ** 2
+
+    a = numerateur / denominateur
+    b = y_bar - a * x_bar
+
+    print("a =", a)
+    print("b =", b)
+    print("Equation : y =", a, "x +", b)
     input("Appuyez sur Entrée pour revenir au menu principal...")
     mainMenu()
 
